914d3fe0a693de89a6a56eb3be5d4abbdeb504e3
Manuel Mauky <manuel.mauky@googlemail.com>
2012-05-01 19:10:55 +0200

    Added Logback

--- src/main/java/eu/lestard/colorpuzzle/Main.java
+++ src/main/java/eu/lestard/colorpuzzle/Main.java
@@ -1,5 +1,8 @@
 package eu.lestard.colorpuzzle;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import eu.lestard.colorpuzzle.core.GameLogic;
 import eu.lestard.colorpuzzle.core.Grid;
 import eu.lestard.colorpuzzle.util.ColorChooser;
@@ -8,19 +11,25 @@ import eu.lestard.colorpuzzle.view.swing.SwingGUI;
 
 
 public class Main {
+
+	private static final Logger log = LoggerFactory.getLogger(Main.class);
+
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		new Main().go();	
+	public static void main(final String[] args) {
+		new Main().go();
 	}
-	
-	public void go(){
-		Grid grid = new Grid(Configurator.getHeight(),Configurator.getWidth(),new ColorChooser(Configurator.getColors()));
-		
+
+	public void go() {
+		log.info("ColorPuzzle started");
+
+		Grid grid = new Grid(Configurator.getHeight(), Configurator.getWidth(),
+				new ColorChooser(Configurator.getColors()));
+
 		GameLogic logic = new GameLogic(grid);
-		
+
 		new SwingGUI(logic).createGui();
 	}
-	
+
 }
