8b7ff5133c1bb4b89635289963d5df899413dcca
Manuel Mauky <manuel.mauky@googlemail.com>
2012-05-20 22:46:39 +0200

    fixed assembly, added licence and readme

---
+++ README.md
@@ -0,0 +1,25 @@
+ColorPuzzle
+===========
+
+Object of the Game
+------------------
+
+The playing field consists of 11*11 squares, randomly filled with one of 7 colors.
+At the start of the game the player "controls" the square at the top left corner.
+By clicking one of the color-buttons on the right he can change the color of all squares he controls.
+
+Every square that has the same color as the player clicks and is in the direct neighborhood of
+ one of the squares controlled by the player, is now also under his control.
+
+The goal is to get all squares on the field under your control with as few clicks as possible.
+
+Build the game
+--------------
+The project is maven based. To build the project simply run 'mvn clean install'. 
+After this run 'mvn assembly:single' to create the executable jar and package all needed libraries into a 'zip' located in the 'target' folder.
+
+
+
+
+
+
--- src/main/java/eu/lestard/colorpuzzle/util/Configurator.java
+++ src/main/java/eu/lestard/colorpuzzle/util/Configurator.java
@@ -13,33 +13,33 @@ import eu.lestard.colorpuzzle.core.Shapes;
 
 @Deprecated
 public class Configurator {
-	
-	
+
+
 	private static int colorCount = 7;
-	
+
 	private static Shapes shape;
 	private static int width = 11;
 	private static int height = 11;
-	
+
 	private static List<Color> colors;
-	
-	private static Map<ColorProfile,List<Color>> colorProfiles;
+
+	private static Map<ColorProfile, List<Color>> colorProfiles;
 	private static ColorProfile colorProfile = ColorProfile.pastel;
-	
-//	private static ArtificialIntelligence ai = new BogoSolver();
+
+	// private static ArtificialIntelligence ai = new BogoSolver();
 	private static ArtificialIntelligence ai = new BruteForceSolver();
-	
-	private Configurator(){
+
+	private Configurator() {
 	}
-	
+
 	private static int aiDelay = 200;
 
-	static{
-		colorProfiles = new HashMap<ColorProfile,List<Color>>();
-		
-		//Default Colors
+	static {
+		colorProfiles = new HashMap<ColorProfile, List<Color>>();
+
+		// Default Colors
 		colors = new ArrayList<Color>();
-		
+
 		colors.add(Color.red);
 		colors.add(Color.blue);
 		colors.add(Color.yellow);
@@ -49,72 +49,73 @@ public class Configurator {
 		colors.add(Color.orange);
 
 		colorProfiles.put(ColorProfile.neon, colors);
-		
+
 		colors = new ArrayList<Color>();
-		
-		colors.add(new Color(255,255,255));		
-		colors.add(new Color(192,192,192));		
-		colors.add(new Color(128,128,128));		
-		colors.add(new Color(64,64,64));		
-		colors.add(new Color(0,0,0));
-		colors.add(new Color(224,224,224));		
-		colors.add(new Color(160,160,160));		
-		
+
+		colors.add(new Color(255, 255, 255));
+		colors.add(new Color(192, 192, 192));
+		colors.add(new Color(128, 128, 128));
+		colors.add(new Color(64, 64, 64));
+		colors.add(new Color(0, 0, 0));
+		colors.add(new Color(224, 224, 224));
+		colors.add(new Color(160, 160, 160));
+
 		colorProfiles.put(ColorProfile.blackWhite, colors);
-		
+
 		colors = new ArrayList<Color>();
-		colors.add(new Color(135,206,250));
-		colors.add(new Color(155,205,50));
-		colors.add(new Color(238,221,130));
-		colors.add(new Color(244,164,96));
-		colors.add(new Color(255,99,71));
-		colors.add(new Color(160,82,45));
-		colors.add(new Color(139,136,120));
-		
-		
+		colors.add(new Color(135, 206, 250));
+		colors.add(new Color(155, 205, 50));
+		colors.add(new Color(238, 221, 130));
+		colors.add(new Color(244, 164, 96));
+		colors.add(new Color(255, 99, 71));
+		colors.add(new Color(160, 82, 45));
+		colors.add(new Color(139, 136, 120));
+
+
 		colorProfiles.put(ColorProfile.pastel, colors);
-		
+
 	}
-	
-	public static String getWinMessage(){
-		return "Gewonnen";
+
+	public static String getWinMessage() {
+		return "Win";
 	}
-	
-	public static void setColorProfile(ColorProfile profile){
+
+	public static void setColorProfile(final ColorProfile profile) {
 		Configurator.colorProfile = profile;
 	}
-	
-	public static void setColorsCount(int colorCount){
+
+	public static void setColorsCount(final int colorCount) {
 		Configurator.colorCount = colorCount;
 	}
+
 	public static int getColorCount() {
 		return colorCount;
 	}
-	
+
 	public static List<Color> getColors() {
-		
+
 		List<Color> list = new ArrayList<Color>();
-		
+
 		int counter = 0;
-		for(Color c : colorProfiles.get(colorProfile)){
-			if(counter < Configurator.colorCount){
+		for (Color c : colorProfiles.get(colorProfile)) {
+			if (counter < Configurator.colorCount) {
 				list.add(c);
 				counter++;
 			}
 		}
-		
+
 		return list;
 	}
-	
-	public static void setWidth(int width){
+
+	public static void setWidth(final int width) {
 		Configurator.width = width;
 	}
-	
+
 	public static int getWidth() {
 		return width;
 	}
 
-	public static void setHeight(int height){
+	public static void setHeight(final int height) {
 		Configurator.height = height;
 	}
 
@@ -123,15 +124,15 @@ public class Configurator {
 	}
 
 
-	public static Shapes getShape(){
+	public static Shapes getShape() {
 		return shape;
 	}
-	
-	public static void setAI(ArtificialIntelligence ai){
+
+	public static void setAI(final ArtificialIntelligence ai) {
 		Configurator.ai = ai;
 	}
-	
-	public static ArtificialIntelligence getAI(){
+
+	public static ArtificialIntelligence getAI() {
 		return ai;
 	}
 
@@ -139,7 +140,7 @@ public class Configurator {
 		return aiDelay;
 	}
 
-	public static void setAiDelay(int aiDelay) {
+	public static void setAiDelay(final int aiDelay) {
 		Configurator.aiDelay = aiDelay;
 	}
 
